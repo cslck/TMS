f2b057262e663ab7b93aa8b4772130a8e8d18d24
admin <admin@admin-PC>
2019-04-09 16:56:40 +0800

    修改放映机的注释信息

--- src/com/csl/dao/ProjectorDao.java
+++ src/com/csl/dao/ProjectorDao.java
@@ -6,14 +6,14 @@ import com.csl.pojo.Projector;
 
 public interface ProjectorDao {
 
-	void insertProjector(Projector pj);//注册影厅信息
+	void insertProjector(Projector pj);//注册放映机信息
 	
-	List<Projector> searchProjectors();//所有影厅信息
+	List<Projector> searchProjectors();//所有放映机信息
 	
 	Projector searchProjectorById(int id);//id查询
 	Projector searchProjectorByHid(int id);//h_id查询
 	
-	void updateProjector(Projector pj);//影厅信息更新
+	void updateProjector(Projector pj);//放映机信息更新
 	
 	void deleteProjectorById(int id);//id删除
 	
